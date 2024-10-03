9e5092aa82a8fcd06ed17ef6234accc9257936ee
melkorAMP <melkor@aleph.engineering>
2024-10-03 00:39:45 -0400

    #2 Update data transformer function algorithm

--- src/utils/dataTransformer.ts
+++ src/utils/dataTransformer.ts
@@ -1,5 +1,6 @@
 export function transformMakeData(makeXml: any): any {
-  const makes = makeXml.Response.Results[0].AllVehicleMakes.map((make: any) => ({
+  const results = makeXml.Response?.Results?.[0]?.AllVehicleMakes || [];
+  const makes = results.map((make: any) => ({
     MakeID: parseInt(make.Make_ID[0], 10),
     MakeName: make.Make_Name[0],
   }));
@@ -7,7 +8,8 @@ export function transformMakeData(makeXml: any): any {
 }
 
 export function transformVehicleTypeData(vehicleTypeXml: any): any {
-  const vehicleTypes = vehicleTypeXml.Response.Results[0].VehicleTypesForMakeIds.map((type: any) => ({
+  const results = vehicleTypeXml.Response?.Results?.[0]?.VehicleTypesForMakeIds || [];
+  const vehicleTypes = results.map((type: any) => ({
     VehicleTypeID: parseInt(type.VehicleTypeId[0], 10),
     VehicleTypeName: type.VehicleTypeName[0],
   }));
