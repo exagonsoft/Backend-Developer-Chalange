615cf40bfb373b48a8f28ffcae4d1ed7eca236c1
melkorAMP <melkor@aleph.engineering>
2024-10-03 17:30:32 -0400

    #5 Implement Logger class

---
+++ src/utils/logger.ts
@@ -0,0 +1,24 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'vehicle-service' },
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(format.colorize(), format.simple()),
+  }));
+}
+
+export default logger;
