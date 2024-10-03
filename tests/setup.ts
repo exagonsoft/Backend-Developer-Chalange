68678caf7fef023759e84c4ac3680a273f939df9
melkorAMP <melkor@aleph.engineering>
2024-10-03 17:07:18 -0400

    #6 Modify Jest setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,8 +1,7 @@
 import prisma from '../src/config/config';
 
 beforeAll(async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE "Make" RESTART IDENTITY CASCADE;`;
-  await prisma.$executeRaw`TRUNCATE TABLE "VehicleType" RESTART IDENTITY CASCADE;`;
+  await prisma.$connect();
 });
 
 afterAll(async () => {
