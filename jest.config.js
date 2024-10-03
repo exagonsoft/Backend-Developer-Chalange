28bd78ab954d1fe2d744cec8e5755b8163a127bc
melkorAMP <melkor@aleph.engineering>
2024-10-03 15:17:32 -0400

    #6 Update Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
+    setupFilesAfterEnv: ['./tests/setup.ts'],
+    globals: {
+      "ts-jest": {
+        "diagnostics": {
+          "warnOnly": true
+        }
+      }
+    }
 };
