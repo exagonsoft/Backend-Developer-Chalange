94ef5a6db6efbe65234ee4df0a7aa65c2642679c
melkorAMP <melkor@aleph.engineering>
2024-10-05 14:26:33 -0400

    #4 Ensure high concurrency

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,13 @@
 
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL || "postgres://default:lbx7wrmQ8HYd@ep-dark-cake-a4igc6ta-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connection_limit=1000"
+      }
+    },
+    log: ['error']
+  });
 
 export default prisma;
